98a90cc8f921588c1cde08a66ccf0e92dc237bc8
Piotr Piasecki <piterpiasecki@gmail.com>
2022-08-30 11:32:52 +0200

    Clear server commit

---
+++ Billing/Server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def run():
+    app.run(host="0.0.0.0")
+@app.route("/")
+def home():
+    return "Server is running"
+
+# @app.route("/health", methods = ["GET"])
+# def health():
+#     return "OK"
+
+# @app.route("/", methods = ["POST"])
+# def
+
+# @app.route("/", methods = ["PUT"])
+# def
+
+if __name__ == "__main__":
+    run()
