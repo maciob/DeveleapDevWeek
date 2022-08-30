0b75a3d30fe551e305b223a93f043e0524d175bb
Piotr Piasecki <piterpiasecki@gmail.com>
2022-08-30 12:20:31 +0200

    Upated health and provider methods

--- Billing/Server.py
+++ Billing/Server.py
@@ -1,26 +1,30 @@
+#!/usr/bin/env python3
 from flask import Flask
 
 app = Flask(__name__)
 
-
 def run():
     app.run(host="0.0.0.0")
-
-
+    
 @app.route("/")
 def home():
     return "Server is running"
 
+@app.route("/health", methods = ["GET"])
+def health():
+    #here we should add checking connection to database
+    return "OK"
 
-# @app.route("/health", methods = ["GET"])
-# def health():
-#     return "OK"
+@app.route("/provider", methods = ["POST"])
+def add_provider():
+    #here we should add creating new provider
+     #it should return a unique provider id as json: { "id":<str>}
+    return 0
 
-# @app.route("/", methods = ["POST"])
-# def
-
-# @app.route("/", methods = ["PUT"])
-# def
+@app.route("/provider<id>", methods = ["PUT"])
+def update_provider(id):
+    #here we should add updating provider
+    return 0
 
 if __name__ == "__main__":
     run()
