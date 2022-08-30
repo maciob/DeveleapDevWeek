25181e628dea4f6f2dea0143a2179fa7d7d2cfd8
Adam Stegienko <107151098+adam-stegienko@users.noreply.github.com>
2022-08-30 14:47:51 +0200

    flask--db linking unfinished

--- Billing/init.sh
+++ Billing/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+docker network create billing_network
 
 docker build -t billing_server:1.0 app/.
-docker run -d --name=flask_server -p 8000:5000 billing_server:1.0
+docker run -d --name=flask_server --network=billing_network -p 8080:5000 billing_server:1.0
 
 docker build -t mysql_db:1.0  db/.
-docker run -d --name=db_server -p 8001:3606 mysql_db:1.0
+docker run -d --name=db_server --network=billing_network -p 8081:3606 mysql_db:1.0
