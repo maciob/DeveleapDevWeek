922c4df797e7fb24bbd83390edfc634abaed690f
jcobzajac <wicket75@interia.pl>
2022-08-30 16:41:49 +0200

    Functionality for csv files and whole logic

--- Weight/app-weight.py
+++ Weight/app-weight.py
@@ -60,22 +60,46 @@ def testdb():
             return render_template("health.html", content="OK")
     
 
- 
-db =  mysql.connector.connect(user='root', host='172.17.0.3', port='3306', password='password', database='weight')
-cur = db.cursor()
+def handling_files(file):
+    db =  mysql.connector.connect(user='root', host='172.17.0.3', port='3306', password='password', database='weight') #Data for changing, cause of different cases
+    cur = db.cursor()
 
-input = "container2.json"
-index = input.index(".")
-extension = input[index:index+5]
+    input = file
+    index = input.index(".")
+    extension = input[index:index+5]
 
-def json_parsing():
-    f = open(input)
-    data = json.load(f)
-    for element in data:
-        id = element["id"]
-        weight = element["weight"]
-        unit = element["unit"]
-        cur.execute("INSERT INTO containers_registered(container_id,weight,unit) VALUES (%s, %s, %s)", (id,weight,unit))
+    def json_parsing():    #Handling json_files
+        f = open(input)
+        data = json.load(f)
+        for element in data:
+            id = element["id"]
+            weight = element["weight"]
+            unit = element["unit"]
+            cur.execute("INSERT INTO containers_registered(container_id,weight,unit) VALUES (%s, %s, %s)", (id,weight,unit))
+
+    def csv_parsing():      #Handling csv files
+        with open(input) as file:
+            content = file.readlines()
+            unit = content[0].split(",")[1][1:3]
+            for num in range (1,len(content) -1):
+                row = content[num].split(",")
+                print(row[0])
+                print(row[1])
+                cur.execute("INSERT INTO containers_registered(container_id,weight,unit) VALUES (%s, %s, %s)", (row[0],row[1],unit))
+                
+
+
+#Logic of function
+    def extract_data():
+        if extension == ".json":
+            json_parsing()
+        elif extension == ".csv":
+            csv_parsing()
+        else:
+            print("File with bad extension")
+
+#Execution of logic
+    extract_data()
        
 
 
