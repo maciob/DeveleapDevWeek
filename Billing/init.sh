b7de017f7434761b46b9290bac986ad6bb084220
Aleks <a.m.malejka@gmail.com>
2022-08-30 14:08:04 +0200

    Changes for DB container in init.sh

--- Billing/init.sh
+++ Billing/init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build -t billing_server:1.0 .
-docker run -d --name=flask_server -p 8080:5000 billing_server:1.0
+docker build -t billing_server:1.0 app/.
+docker run -d --name=flask_server -p 8000:5000 billing_server:1.0
+
+docker build -t mysql_db:1.0  db/.
+docker run -d --name=db_server -p 8001:3606 mysql_db:1.0
