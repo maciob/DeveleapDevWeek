9ad13bd8429913c7c2f9e59e46f888f854ac54e1
Adam Stegienko <107151098+adam-stegienko@users.noreply.github.com>
2022-08-30 11:48:37 +0200

    Index.html blueprint added and connected:

--- Billing/billing.py
+++ Billing/billing.py
@@ -1,17 +1,11 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template, request, redirect, jsonify
 
 app = Flask(__name__)
 
-@app.route("/provider", methods=["POST"])
-def add_new_provider():
+@app.route("/", methods=["GET"])
+def home():
     return render_template("index.html")
-
-@app.rout("/provider/<id>", methods=["PUT"])
-def update_provider():
-    return render_template("index.html")
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
