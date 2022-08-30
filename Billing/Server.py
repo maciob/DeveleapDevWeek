de1d7530f5fccf2eb6b9943c164682d112955e99
Piotr Piasecki <piterpiasecki@gmail.com>
2022-08-30 11:40:54 +0200

    Created first methods templates

--- Billing/Server.py
+++ Billing/Server.py
@@ -2,11 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 def run():
     app.run(host="0.0.0.0")
+
+
 @app.route("/")
 def home():
     return "Server is running"
+
 
 # @app.route("/health", methods = ["GET"])
 # def health():
