5ba8a14034e4a92e48a529a5fb203d2e0cea8ace
Adam Stegienko <107151098+adam-stegienko@users.noreply.github.com>
2022-08-31 23:08:43 +0200

    cosmetic changes: Dockerfile, dockerignore, init.sh: rates table rendered now

--- Billing/init.sh
+++ Billing/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build -t billing_server:1.0 app/.
-docker run -d --name=flask_server -v $(pwd)/app/in:/app/in -p 8086:5000 billing_server:1.0
+docker run -d --name=flask_server -v $(pwd)/app/in/:/app/in/ -p 8086:5000 billing_server:1.0
 
 docker build -t mysql_db:1.0  db/.
 docker run -d --name=db_server -v $(pwd)/db_volume:/var/lib/mysql -p 8087:3606 mysql_db:1.0
