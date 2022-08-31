359d2cafb61e027718b5fc63995719c5e289ae19
Adam Stegienko <107151098+adam-stegienko@users.noreply.github.com>
2022-08-31 14:09:43 +0200

    Current working version with volumes

--- Billing/init.sh
+++ Billing/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker build -t billing_server:1.0 app/.
-docker run -d --name=flask_server  -p 8086:5000 billing_server:1.0
+docker run -d --name=flask_server -v $(pwd)/app/in:/app/in -p 8086:5000 billing_server:1.0
 
 docker build -t mysql_db:1.0  db/.
-docker run -d --name=db_server  -p 8087:3606 mysql_db:1.0
+docker run -d --name=db_server -v billingVolume:/var/lib/mysql -p 8087:3606 mysql_db:1.0
 
 sleep 15
 
