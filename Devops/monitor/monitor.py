7ffbf5c9a2e70580342566c9c6c695e704e74c8c
Ubuntu <ubuntu@ip-10-0-0-6.eu-west-2.compute.internal>
2022-09-01 06:43:27 +0000

    Resolved conflicts

--- Devops/monitor/monitor.py
+++ Devops/monitor/monitor.py
@@ -1,38 +1,24 @@
-from flask import Flask, jsonify
-from datetime import datetime
-import requests
-from flask import request
-import json
+from contextlib import closing
+from flask import Flask
 import socket
-import os
 
 app = Flask(__name__)
-
-lista = []
-
-testingflag = False
+HOST = "localhost"
+PORT = [8080, 8081, 8082, 8083, 8084, 8085, 8086, 8087, 8088, 8089]
 
 
 @app.route("/home")
 def home():
-	HOST = "localhost"
-	PORT = 8080
 
-	# Creates a new socket
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	# Try to connect to the given host and port
-	if sock.connect_ex((HOST, PORT)) == 0:
-		lista.append("Port " + str(PORT) + " is open") # Connected successfully
-	else:
-		lista.append("Port " + str(PORT) + " is closed") # Failed to connect because port is in use (or bad host)
-
-	# Close the connection
-	sock.close()
-
-	return lista
+    port_list = []
+    for port in PORT:
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+            if sock.connect_ex((HOST, port)) == 0:
+                port_list.append(f"<li>Service at port {str(port)} is up </li><br>")
+            else:
+                port_list.append(f"<li>Service at port {str(port)} is down </li><br>")
+    return "".join(port_list)
 
 
 if __name__ == "__main__":
-	testingflag = False
-	app.run(host="0.0.0.0", port=8088)
+    app.run(host="0.0.0.0", port=8088)
