9ee9c252a94feac3fae27a8c10478fbb0551fb29
Ubuntu <ubuntu@ip-10-0-0-14.eu-west-2.compute.internal>
2022-09-01 09:37:34 +0000

    minor changes

--- Devops/tracker/app.py
+++ Devops/tracker/app.py
@@ -1,12 +1,8 @@
-from flask import Flask, jsonify
-from datetime import datetime
-import requests
-from flask import request
+from flask import Flask, jsonify, request
 import json
-import time
 import os
 import threading
-
+import time
 
 app = Flask(__name__)
 
@@ -15,6 +11,11 @@ lock = threading.Lock()
 
 testingflag = False
 
+@app.route("/monitor",methods=["POST"])
+def health():
+    r = request.data.decode('utf-8')
+    return jsonify(success=True)
+
 
 @app.route("/api", methods=["POST"])
 def list():
@@ -22,12 +23,17 @@ def list():
     r = request.json
     before = r["before"]
     after = r["after"]
-    lista.append(before)
-    lista.append(after)
+    branch = r["ref"]
+    #lista.append(before)
+    #lista.append(after)
+    #lista.append(branch)
     lock.acquire()
     #os.system("git clone https://github.com/maciob/DeveleapDevWeek git")
-    os.system("docker -H tcp://0.0.0.0:9000 ps -a")
-    #time.sleep(20)  # testingphase
+    lista.append(before)
+    lista.append(after)
+    lista.append(branch)
+    os.system("docker ps -a")
+    time.sleep(20)  # testingphase
     lock.release()
     return jsonify(success=True)
 
