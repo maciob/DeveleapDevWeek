c3a356a0e78127515a64fbd7c537fbf78bcc83d9
Adam Stegienko <107151098+adam-stegienko@users.noreply.github.com>
2022-09-01 15:13:58 +0200

    Tests fixed

--- Billing/tests/get_health.sh
+++ Billing/tests/get_health.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd ../ > /dev/null
-bash -c ./init.sh
+command ./init.sh "test"
 popd > /dev/null
 
 sleep 2
--- Billing/tests/put_provider_id.sh
+++ Billing/tests/put_provider_id.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 pushd ../ > /dev/null
-bash -c ./init.sh
+command ./init.sh "test"
 popd > /dev/null
 
-sleep 5
+sleep 2
 
-curl -d "username=test_when user doesnt_exist" -X PUT localhost:8086/provider/99 | tac | tac | grep -a "No provider" &>/dev/null
+curl -d "username=test_when user does not_exist" -X PUT localhost:8086/provider/99 | tac | tac | grep -a "No provider" &>/dev/null
 if [ $? == 0 ]; then
-    echo "Passed when user doesnt exist."
+    echo "Passed when user does not exist."
 else
     echo "Failed."
 fi
 
-curl -d "username=test_when user_exist" -X PUT localhost:8086/provider/10001 | tac | tac | grep -a "test_when user_exist" &>/dev/null
+curl -d "username=test_when user_exists" -X PUT localhost:8086/provider/10001 | tac | tac | grep -a "test_when user_exist" &>/dev/null
 if [ $? == 0 ]; then
-    echo "Passed when user exist."
+    echo "Passed when user exists."
 else
     echo "Failed."
 fi
