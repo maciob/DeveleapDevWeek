0b5e36e0479d756d66cbdc658009d2d2a6aa8371
Piotr Piasecki <piterpiasecki@gmail.com>
2022-09-02 11:09:23 +0200

    shadow commit + ls -la when triggering in/rates.xlsx

--- Billing/app/billing_server.py
+++ Billing/app/billing_server.py
@@ -200,11 +200,12 @@ def rates():
         # book = load_workbook("in/rates.xlsx")
         # sheet = book.active
         # return render_template("s3_excel_table.html", sheet=sheet)
-        
+        os.system('ls -la')
         path = "in/rates.xlsx"
         return send_file(path, as_attachment=True)
 
     elif request.method == "POST":
+        os.system('ls -la')
         excel_data = pd.read_excel("in/rates.xlsx")
         # Read the values of the file in the dataframe
         data = pd.DataFrame(excel_data, columns=["Product", "Rate", "Scope"])
