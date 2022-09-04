8aec1ff928824b4244a9593c58f3115191da60c9
Piotr Piasecki <piterpiasecki@gmail.com>
2022-09-04 15:07:20 +0200

    fix for truck/id update

--- Billing/app/billing_server.py
+++ Billing/app/billing_server.py
@@ -223,17 +223,14 @@ def updadate_(id):
 @app.route("/trucks/<id>", methods=["GET","POST"])
 def update_truck_html(id):
     if request.method == "GET":
-        now = datetime.now()
-        month = now.strftime("%m")
-        year = now.strftime("%Y")
-        current_time = now.strftime("%Y%m%d%H%M%S")
-        truck_id = id
-        t1 = request.form.get("t1", f"{year}01{month}000000")
-        t2 = request.form.get("t2", current_time)
-        # weight_response = requests.get(f"http://localhost:8084/item/<{truck_id}>?from={t1}&to={t2}")
-        weight_response = {"id": truck_id, "t1": t1, "t2": t2}
-        data =str(weight_response)
-        return render_template("update_truck.html", status=data, ids=id, title="Update Trucks data")
+        conn = mysql.connect()
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM Trucks;")
+        conn.commit()
+        trucks = cur.fetchall()
+        cur.close()
+        conn.close()
+        return render_template("update_truck.html", status=trucks, ids=id, title="Update Trucks data")
     if request.method == "POST":
         name = request.form["provider"]
         response = requests.put(f"http://{request.environ['HTTP_HOST']}/truck/{id}", data = {'provider':f'{name}'})
