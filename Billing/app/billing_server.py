644ab3c526c64d9bc4fa80ef56f3372777b12780
Adam Stegienko <107151098+adam-stegienko@users.noreply.github.com>
2022-09-05 10:23:01 +0200

    Readme updated + 404/500 error handling

--- Billing/app/billing_server.py
+++ Billing/app/billing_server.py
@@ -351,6 +351,16 @@ def prates():
         response = requests.post(f"http://{request.environ['HTTP_HOST']}/rates")
         return response.content
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # 404 - Page Not Found
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    # 500 - Internal Server Error
+    return render_template('500.html'), 500
+
 
 if __name__ == "__main__":
     run()
